6bdc077f624abd26270959be80937e9026482f45
vishal0078380 <vishal733728@gmail.com>
2025-09-30 17:55:51 +0530

    Add files via upload

---
+++ backend/middleware/auth.js
@@ -0,0 +1,16 @@
+//code for authentication users
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async function (req, res, next) {
+  const token = req.header('Authorization')?.split(' ')[1];
+  if (!token) return res.status(401).json({ msg: 'No token, authorization denied' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+   
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
